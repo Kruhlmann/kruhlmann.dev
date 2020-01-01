f6468de610ca9eea339e69ae742c1b939a5549ba
Andreas Kruhlmann <kruhlmann@protonmail.com>
2020-01-01 20:13:31 +0100

    No more code backdrop

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,29 +1,20 @@
-<script context="module">
-    export async function preload() {
-        this.fetch("/api/code_backdrop").then((response) => {
-            return response.json();
-        }).then((response) => {
-            console.log(response);
-        });
-    }
-</script>
+<div class="container">
+    <div class="backdrop">
+        <nav>
+            <div class="filler"></div>
+            <a href="/portfolio" class="item" rel="prefetch">
+                Portfolio
+            </a>
+            <a href="/resume" class="item" rel="prefetch">
+                Resume
+            </a>
+        </nav>
 
-<div class="backdrop">
-    <nav>
-        <div class="filler"></div>
-        <a href="/portfolio" class="item" rel="prefetch">
-            Portfolio
-        </a>
-        <a href="/resume" class="item" rel="prefetch">
-            Resume
-        </a>
-    </nav>
-
-    <div class="content">
-        <slot/>
+        <div class="content">
+            <slot/>
+        </div>
     </div>
 </div>
-
 
 <style lang="scss">
 @import "../scss/main";
@@ -39,29 +30,63 @@
     }
 }
 
-.backdrop {
-    display: flex;
-    flex-direction: column;
+:global(#sapper) {
+    height: 100%;
+}
 
-    nav {
-        color: $nav-color;
-        font-size: 18px;
+.container {
+    position: relative;
+    height: 100%;
+
+    .backdrop-source {
+        height: 100%;
         width: 100%;
-        display: grid;
-        grid-template-columns: 1fr auto auto;
-        grid-template-rows: 1fr;
-        padding: 15px 30px;
-        box-sizing: border-box;
-        font-family: "Input Mono",monaco,monospace;
+        position: absolute;
+        top: 0;
+        right: 0;
+        display: flex;
+        align-items: center;
 
-        .item {
-            padding: 15px;
-            text-transform: uppercase;
+        textarea {
+            font-family: Courier;
+            height: 80%;
+            margin: 0 auto;
+            border: none;
+            resize: none;
+            font-size: 18px;
+            background: none;
+            overflow: hidden;
         }
     }
 
-    .content {
+    .backdrop {
+        display: flex;
+        flex-direction: column;
         flex: 1;
+        height: 100%;
+        position: relative;
+        z-index: 1;
+
+        nav {
+            color: $nav-color;
+            font-size: 18px;
+            width: 100%;
+            display: grid;
+            grid-template-columns: 1fr auto auto;
+            grid-template-rows: 1fr;
+            padding: 15px 30px;
+            box-sizing: border-box;
+            font-family: "Input Mono",monaco,monospace;
+
+            .item {
+                padding: 15px;
+                text-transform: uppercase;
+            }
+        }
+
+        .content {
+            flex: 1;
+        }
     }
 }
 
