61eb2dd193dafd4011100d473ebc9401c17e6e5f
Andreas Krühlmann <kruhlmann@protonmail.com>
2020-01-02 12:52:27 +0100

    Syntax error

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -149,7 +149,7 @@
                 display: flex;
                 flex-direction: column;
                 justify-content: center;
-                padidng: 15px 0;
+                padding: 15px 0;
 
                 .item {
                     display: grid;
