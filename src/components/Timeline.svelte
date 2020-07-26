afe48a9ce2fc95b92b499496bda8e6f945bc132b
Andreas Krühlmann <andreas@kruhlmann.dev>
2020-07-26 03:44:35 +0200

    :recycle: Fixed constructor complexity

---
+++ src/components/Timeline.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    const items = [
+        {
+            date: { year: 2015, month: 2, day: 16 },
+            type: "employment_enter",
+            title: "Siemens Mobility A/S",
+            description:
+                "Worked at a student worker in the student talent pool of Siemens Mobility A/S",
+            technologies: [
+                "C#",
+                "Java",
+                "VB6",
+                "JavaScript",
+                "CSS/SCSS",
+                "SQL",
+                "Python",
+            ],
+        },
+    ];
+</script>
---
+++ src/routes/portfolio.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Timeline from "../components/Timeline.svelte";
+</script>
+
+<Timeline />
+
+<style lang="scss">
+
+</style>
